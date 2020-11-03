43d092a8850a49c94213f99f9f729e7914094159
Abdullah Numan <anewman15@hotmail.com>
2020-11-03 19:41:40 +0600

    Generate registrations and sessions controllers for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 end
