2ca148f1c7993be1334b50f5064660cc59daa75e
Abdullah Numan <anewman15@hotmail.com>
2020-11-04 14:32:03 +0600

    Style users/registrations#new view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     form.input :remember_me, as: :boolean if devise_mapping.rememberable?
   end
 
+  def minimum_chars_hint(minimum_password_length)
+    "#{minimum_password_length} characters minimum" if minimum_password_length
+  end
+
 end
