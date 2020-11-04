eba62a8d106f5e302382816ebc9b85689ca1f8b5
Abdullah Numan <anewman15@hotmail.com>
2020-11-04 13:25:50 +0600

    Define new routes, set root, redirect undefined actions to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
+  resources :users do
+    resources :activities, except: [ :show, :destroy ]
+    resources :groups, only: [ :new, :create, :index ]
+  end
+
+  devise_scope :user do
+    root 'users/sessions#new'
+  end
+
+  get "*path", to: redirect('/')
 end
