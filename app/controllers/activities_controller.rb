a82aedacec66f536cdedae00467352709eed3d9f
Abdullah Numan <anewman15@hotmail.com>
2020-11-05 15:55:34 +0600

    Create new, create and index actions, form and views for activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,16 @@
 class ActivitiesController < ApplicationController
+
+  def index
+    @activity = Activity.new
+    @activities = current_user.activities
+    @groups = Group.all
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = current_user.activities.build()
+  end
 end
