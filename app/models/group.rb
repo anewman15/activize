ef9395612160a8df0cd3b08ba234a3883701dc4e
Abdullah Numan <anewman15@hotmail.com>
2020-11-07 17:39:23 +0600

    Add validation for the presence of :icon in Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   validates :name, presence: true, length: { in: 3..100 }
+  validates :icon, presence: true
   validates_uniqueness_of :name, scope: :author_id
 
   belongs_to :author, class_name: 'User'
