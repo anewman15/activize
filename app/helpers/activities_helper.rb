417ee914f7b8c44be280aba92dd6230eb3ffb548
Abdullah Numan <anewman15@hotmail.com>
2020-11-08 13:55:23 +0600

    Create helper for activity image logic

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,10 @@
 module ActivitiesHelper
+  def activity_image_src(activity)
+    if activity.group.present?
+      url_for(activity.group.icon) if activity.group.icon.attached?
+    else
+      "https://bulma.io/images/placeholders/128x128.png"
+    end
+  end
+
 end
